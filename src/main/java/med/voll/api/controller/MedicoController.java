42511720be3905b5c489c646711e906b517719aa
Allan <allandrs@gmail.com>
2023-12-03 12:05:05 -0300

    wip porject

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,8 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar() {
-        //return "teste";
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
+
 
     }
 
