3f5a36f5e48b970f28bc418bad6ba8516450e944
Allan <allandrs@gmail.com>
2023-12-08 00:32:20 -0300

    wip project

--- src/main/java/med/voll/api/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/paciente/PacienteRepository.java
@@ -1,6 +1,4 @@
 package med.voll.api.paciente;
-
-import med.voll.api.medico.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
