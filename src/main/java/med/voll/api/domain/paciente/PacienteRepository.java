919b5f948855d63bc29a2506ad42e30a663159f4
Allan <allandrs@gmail.com>
2023-12-15 18:45:31 -0300

    wip project

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,5 +1,4 @@
-package med.voll.api.paciente;
-import io.micrometer.observation.ObservationFilter;
+package med.voll.api.domain.paciente;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
