141782c097e926a86de1b2303c3748ba93322f5f
Allan <allandrs@gmail.com>
2023-12-19 14:34:04 -0300

    starting configure spring security

--- src/main/java/med/voll/api/infra/TratadorErros.java
+++ src/main/java/med/voll/api/infra/TratadorErros.java
@@ -1,7 +1,10 @@
 package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.FieldResult;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,4 +15,17 @@ public class TratadorErros {
     public ResponseEntity tratarErro404(){
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
 }
